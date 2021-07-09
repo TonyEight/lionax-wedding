d840aa12e86e0101c052a959c0d1522667d925e8
TonyEight <github@tonyeightgeek.diff-air.com>
2021-07-09 12:29:13 +0200

    Refactor frontpage views

--- frontpage/urls.py
+++ frontpage/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = "frontpage"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("information", views.InformationView.as_view(), name="information"),
-    path("wedding-list", views.WeddingListView.as_view(), name="wedding-list"),
+    path("", views.FrontPageGenericView.as_view(), name="index"),
+    path("information", views.FrontPageGenericView.as_view(), name="information"),
+    path("wedding-list", views.FrontPageGenericView.as_view(), name="wedding-list"),
 ]
--- frontpage/views.py
+++ frontpage/views.py
@@ -1,13 +1,18 @@
 from django.views import generic
 
 
-class IndexView(generic.TemplateView):
-    template_name = 'index.html'
+class FrontPageGenericView(generic.TemplateView):
+    index_template = 'index.html'
+    information_template = 'information.html'
+    wedding_list_template = 'wedding-list.html'
 
-
-class InformationView(generic.TemplateView):
-    template_name = 'information.html'
-
-
-class WeddingListView(generic.TemplateView):
-    template_name = 'wedding-list.html'
+    def get_template_names(self):
+        template_names = []
+        from django.urls import reverse
+        if self.request.path == reverse('frontpage:index'):
+            template_names.append(self.index_template)
+        if self.request.path == reverse('frontpage:information'):
+            template_names.append(self.information_template)
+        if self.request.path == reverse('frontpage:wedding-list'):
+            template_names.append(self.wedding_list_template)
+        return template_names
