16790c4f0c84a6b73ca3c39bab02b0758635bea5
TonyEight <github@tonyeightgeek.diff-air.com>
2021-08-10 18:26:27 +0200

    Add covid-19 page, fix noindex and alert rendering

--- frontpage/urls.py
+++ frontpage/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.FrontPageGenericView.as_view(), name="index"),
     path("information", views.FrontPageGenericView.as_view(), name="information"),
     path("wedding-list", views.FrontPageGenericView.as_view(), name="wedding-list"),
+    path("information-covid-19", views.FrontPageGenericView.as_view(), name="covid-19"),
 ]
--- frontpage/views.py
+++ frontpage/views.py
@@ -5,6 +5,7 @@ class FrontPageGenericView(generic.TemplateView):
     index_template = 'index.html'
     information_template = 'information.html'
     wedding_list_template = 'wedding-list.html'
+    covid_template = 'covid-19.html'
 
     def get_template_names(self):
         template_names = []
@@ -15,4 +16,6 @@ class FrontPageGenericView(generic.TemplateView):
             template_names.append(self.information_template)
         if self.request.path == reverse('frontpage:wedding-list'):
             template_names.append(self.wedding_list_template)
+        if self.request.path == reverse('frontpage:covid-19'):
+            template_names.append(self.covid_template)
         return template_names
